e57efbffe26c1b92070954bf7e9e5330d1fef322
Adam Carnicle-Maile <LegoManAdamCM@hotmail.com>
2023-08-14 14:43:04 -0500

    Syl's commit that is really good

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,25 @@
+// Need comment at the top of the program.
+
+// Changes made: Condensed some code, changed most println to just print
+// Nothing changed in Data.java
+// - Syl
 import java.util.Scanner;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Enter the download size: ");
+        System.out.print("Enter the download size: ");
         double dataAmount = Double.parseDouble(input.nextLine());
-
-        System.out.println("Enter the measurement on the download");
-        System.out.println("(bytes, kilobytes, megabytes, gigabytes): ");
+        /*
+            Instead of doing this:
+                System.out.println("Enter the measurement on the download");
+                System.out.println("(bytes, kilobytes, megabytes, gigabytes): ");
+            Do this:
+         */
+        System.out.print("Enter the measurement of the download\n(bytes, kilobytes, megabytes, gigabytes): ");
         String unit = input.nextLine();
 
-        System.out.println("Enter your download speed (Megabits/sec): ");
+        System.out.print("Enter your download speed (Megabits/sec): ");
         int speed = Integer.parseInt(input.nextLine());
 
         //Build a Data object with these three values
